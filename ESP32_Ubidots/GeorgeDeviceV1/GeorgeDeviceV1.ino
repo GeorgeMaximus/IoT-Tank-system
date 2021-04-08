eb85fb1aaa9d46e4e959c25104d620081bdcfadf
George Raouf <georgemaximus0@gmail.com>
2021-04-08 14:46:52 +0200

    It sends data to the cloud and access point working good

--- ESP32_Ubidots/GeorgeDeviceV1/GeorgeDeviceV1.ino
+++ ESP32_Ubidots/GeorgeDeviceV1/GeorgeDeviceV1.ino
@@ -27,6 +27,7 @@
 #include <AutoConnect.h>
 #include <AutoConnectCredential.h>
 #include <PageBuilder.h>
+#include <PubSubClient.h>
 
 #if defined(ARDUINO_ARCH_ESP8266)
 ESP8266WebServer Server;
@@ -37,6 +38,63 @@ WebServer Server;
 AutoConnect      Portal(Server);
 String viewCredential(PageArgument&);
 String delCredential(PageArgument&);
+
+/////////////////////////////////////////////// MQTT Variable for Ubidots 
+//#define WIFISSID "GeorgeMaximus" // Put your WifiSSID here
+//#define PASSWORD "maxi1234" // Put your wifi password here
+#define TOKEN "BBFF-l8gRwzQpEikMaXp3uAahxYodCzOX45" // Put your Ubidots' TOKEN
+#define MQTT_CLIENT_NAME "GRDuncanIoT32DhTTankMaximusesp32" // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string; 
+                                           //it should be a random and unique ascii string and different from all other devices
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define VARIABLE_LABEL "sensor" // Assing the variable label
+#define DEVICE_LABEL "esp32" // Assig the device label
+
+#define SENSOR 12 // Set the GPIO12 as SENSOR
+
+char mqttBroker[]  = "industrial.api.ubidots.com";
+char payload[100];
+char topic[150];
+// Space to store values to send
+char str_sensor[10];
+////////////////////////////////////////////////////////////////////////
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+WiFiClient ubidots;
+PubSubClient client(ubidots);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  String message(p);
+  Serial.write(payload, length);
+  Serial.println(topic);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    
+    // Attemp to connect
+    if (client.connect(MQTT_CLIENT_NAME, TOKEN, "")) {
+      Serial.println("Connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////
+
 
 // Specified the offset if the user data exists.
 // The following two lines define the boundalyOffset value to be supplied to
@@ -152,13 +210,15 @@ String delCredential(PageArgument& args) {
   return "";
 }
 
+/////////////////////////////////////////////////////////////////////////////////////
+
 void setup() {
   delay(1000);
   Serial.begin(115200);
   Serial.println();
 
   rootPage.insert(Server);    // Instead of Server.on("/", ...);
-  delPage.insert(Server);     // Instead of Server.on("/del", ...);
+  //delPage.insert(Server);     // Instead of Server.on("/del", ...); ( have deleted this ) 
 
   // Set an address of the credential area.
   AutoConnectConfig Config;
@@ -170,8 +230,34 @@ void setup() {
     Serial.println("WiFi connected: " + WiFi.SSID());
     Serial.println("WiFi connected: " + WiFi.localIP().toString());
   }
+
+//////////////// MQTT Ubidots Initiaization //////////
+    client.setServer(mqttBroker, 1883);
+  client.setCallback(callback);  
 }
 
 void loop() {
+  DataToCloud();
     Portal.handleClient();
+}
+
+void DataToCloud() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+  sprintf(payload, "%s", ""); // Cleans the payload
+  sprintf(payload, "{\"%s\":", VARIABLE_LABEL); // Adds the variable label
+  
+  float sensor = random(500); 
+  
+  /* 4 is mininum width, 2 is precision; float value is copied onto str_sensor*/
+  dtostrf(sensor, 4, 2, str_sensor);
+  
+  sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+  Serial.println("Publishing data to Ubidots Cloud");
+  client.publish(topic, payload);
+  client.loop();
+  delay(1000);
 }
